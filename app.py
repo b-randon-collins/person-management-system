f3fdc22750934d124947c2c1711e9160a3759333
Brandon Collins <brandonc503@yahoo.com>
2024-09-12 02:30:14 -0400

    view_person_options

--- app.py
+++ app.py
@@ -34,12 +34,16 @@ def main_menu():
             print("Invalid choice! Please try again.")
 
 def add_person_workflow():
+    os.system('clear')
     name = input("Enter the person's name: ")
     dob = input("Enter the person's DOB (optional): ")
 
     person_id = Person.add_person(name, dob) 
 
-    print(f"\nPerson '{name}' added successfully with ID {person_id}!")
+    print(f"\nPerson '{name}' added successfully.")
+    time.sleep(1)
+
+    
 
     while True:
         print("\nWhat would you like to do next?")
@@ -49,12 +53,13 @@ def add_person_workflow():
         choice = input("\n > ")
 
         if choice == '1':
+            view_person_workflow(person_id)
             break
         elif choice == '2':
             add_person_workflow()
             return
         elif choice == '3':
-            return
+            main_menu()
         else:
             print("Invalid choice! Please try again.")
 
@@ -64,7 +69,7 @@ def list_people_workflow():
     for i, person in enumerate(people, start=1):
         print(f"{i}. {person.name}")
 
-    person_id = int(input("\n > "))
+    person_id = int(input("Select person by ID to view details: "))
     view_person_workflow(person_id)
 
 def view_person_workflow(person_id):
@@ -85,14 +90,53 @@ def view_person_workflow(person_id):
         for email in email_addresses:
             print(f"  {email[2]}")  
 
-        return
+        view_person_options(person_id)
     else:
         print("Person not found.")
         time.sleep(1)
         list_people_workflow()
 
 
+def view_person_options(person_id):
+    while True:
+        print("")
+        print("1. Update person")
+        print("2. Delete person")
+        print("3. Return to list")
+        print("4. Return to Main Menu")
 
+        choice = input("\n > ")
+
+        if choice == '1':
+            update_person_options(person_id)
+            view_person_workflow(person_id)
+        elif choice == '2':
+            if input("Are you sure? (y/n): ").lower() == 'y':
+                Person.delete_person(person_id)
+                print("Person deleted.")
+                time.sleep(1)
+                list_people_workflow()
+                return
+            else:
+                view_person_workflow(person_id)
+        elif choice == '3':
+            list_people_workflow()
+            return
+        elif choice == '4':
+            main_menu()
+            return
+        else:
+            print("Invalid choice! Please try again.")
+
+
+def update_person_options(person_id):
+   pass
+
+def update_phone_numbers_options(person_id):
+  pass
+
+def update_email_addresses_options(person_id):
+    pass
 
 if __name__ == "__main__":
     initialize_db()
